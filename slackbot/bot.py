6e29b423fc2fd53e27ec4d1fd97b56cfe84f097a
Suleyman <suleymaneminov22@gmail.com>
2022-01-29 20:39:44 -0500

    add-feature command added

--- slackbot/bot.py
+++ slackbot/bot.py
@@ -19,13 +19,14 @@ firebaseConfig = {
 firebase=pyrebase.initialize_app(firebaseConfig)
 db=firebase.database()
 
-load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'], '/slack/events', app)
+    os.environ.get('SIGNING_SECRET'), '/slack/events', app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=os.environ.get('SLACK_TOKEN'))
 BOT_ID = client.api_call("auth.test")['user_id']
 
 @app.route('/help',methods=['POST'])
@@ -67,11 +68,57 @@ def add_idea():
 
     
     if BOT_ID != user_id:
-
         client.chat_postMessage(channel=channel_id,text=f"{user_name} added an Idea: {idea_message}")
-
     return Response(),200
 
+# show-idea
+@app.route('/add-feature',methods=['POST'])
+
+# add-feature command
+@app.route('/add-feature',methods=['POST'])
+def add_feature():
+    data = request.form
+
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    feature_message = data.get('text')
+    user_name = data.get('user_name')
+    channel_name = data.get('channel_name')
+
+
+    #  retrieve data form the db
+    if db.get().val() is None or channel_name not in db.get().val(): # if new channel
+        db.child(channel_name).set({"count":1})
+        db.child(channel_name).child("features").child(1).set({"description":feature_message})
+
+    else:
+        count=db.child(channel_name).get().val()["count"]+1
+        db.child(channel_name).update({"count":count})
+        db.child(channel_name).child("ideas").child(count).set({"description":feature_message})
+
+
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel_id,text=f"{user_name} added an Idea: {feature_message}")
+    return Response(),200
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
+
+
+
+# TODO: Create /show-ideas slash command - retrieve list of ideas from Firebase db ( 
+#                           (e.g idea1 by user_name1 - Instagram app clone /n 
+#                                idea2 by user_name2 - App that helps to sleep \n 
+#                                idea3 ...  ))
+# TODO: Create /create-project (title, idea-number )
+#           Decide what project hackers will be working on
+# TODO: Create /add-feature command (attr={""})
+#           Add feature to the list of features.
+# TODO: Create /show-features 
+#           Retrieve list of features from Firebase db and post message
+# TODO: Create /help slash command 
+#           Show list of commands 
+# TODO: Create /todo slash command
+#           Add to-do to list todo
+# TODO: Create /assign-task slash command
+# TODO: Create /show-my-tasks slash command
